4936cb9b29a5983670d841fc4c58779531197931
nazma <nazmacyberlink@gmail.com>
2024-01-25 16:19:43 +0545

    homepage 50%

--- index.php
+++ index.php
@@ -1,11 +1,114 @@
 <?php include('assets/include/header.php') ?>
 
 
-<section class="uk-section">
-    <div class="uk-container">
-        
+<section class="uk-hero-banner uk-position-relative uk-flex uk-flex-center uk-flex-middle" style="background-image:url('assets/img/banner.jpg');">
+    <div class="uk-container uk-z3 uk-width-3-4@m">
+        <div class="uk-white-title">
+            <h4 class="uk-text-center">- Where Taste Meets Tradition -</h4>
+            <h1 class="uk-banner-title text-white uk-text-center uk-margin-remove-top">Throw the most epic parties in town!</h1>
+        </div>
+        <div class="uk-margin-medium-top uk-text-center">
+            <a href="" class="uk-btn uk-primary ">Read More</a>
+        </div>
+    </div>
+    <div class="texture">
+        <img src="https://hotelpadma.com.np/wp-content/themes/wp-bootstrap-starter/images/texture.png" alt="">
     </div>
 </section>
 
+<section class="uk-section uk-about uk-padding-remove-horizontal">
+    <div class="uk-container-xlarge uk-padding-remove-horizontal">
+        <div class="uk-grid uk-grid-collapse uk-grid-match">
+            <div class="uk-width-1-5@m" uk-scrollspy="cls: uk-animation-slide-left; repeat: true; delay:200">
+                <img src="assets/img/banner.jpg" alt="">
+            </div>
+            <div class="uk-width-3-5@m uk-padding-small" uk-scrollspy="target:.uk-primary-title,h2,p,.uk-btn; cls: uk-animation-slide-top-medium;   delay: 50; repeat: true;">
+                <div class="uk-padding uk-flex uk-flex-middle uk-flex-column uk-text-center">
+                    <div class="uk-primary-title ">
+                        <h4 class="uk-margin-remove">Why Choose MGC</h4>
+                    </div>
+                    <div class="uk-section-title uk-text-center">
+                        <h2>We Help Re-defining Your Catering Experience</h2>
+                    </div>
+                    <p>MGC Catering and Events is your all-in-one resource for every aspect of your event. Our focus is committed to the details and flawless execution. Our unsurpassed customer service, and genuine passion for helping people make us truly the undisputable top choice. </p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, consequatur. Aliquam quaerat pariatur repellendus veniam nemo, saepe, culpa eius aspernatur cumque suscipit quae nobis illo tempora. Eum veniam necessitatibus, blanditiis facilis quidem dolore! Eum veniam necessitatibus, blanditiis facilis quidem dolore! </p>
+                    <div class="uk-text-center mt-5">
+                        <a href="" class="uk-btn uk-primary ">Read More</a>
+                    </div>
+                </div>
 
-<?php include('assets/include/footer.php') ?> 
+            </div>
+            <div class="uk-width-1-5@m" uk-scrollspy="cls: uk-animation-slide-right; repeat: true; delay:200">
+                <img src="assets/img/banner.jpg" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="uk-section uk-services uk-padding-remove-horizontal">
+    <div class="uk-container-xlarge uk-padding-remove-horizontal">
+        <div class="uk-grid uk-child-width-1-3@m uk-grid-collapse uk-grid-match" uk-scrollspy="target:.uk-title ,h2,p,.uk-btn; cls: uk-animation-slide-top-medium;   delay: 50; repeat: true;">
+            <div>
+                <div class="media">
+                    <img src="assets/img/banner.jpg" alt="">
+                </div>
+            </div>
+            <div class=" uk-services-content">
+                <div class="uk-padding uk-flex uk-flex-middle uk-flex-column uk-text-center">
+                    <div class="uk-title ">
+                        <h4 class="uk-margin-remove">Services</h4>
+                    </div>
+                    <div class="uk-section-title uk-text-center">
+                        <h2>Just Bartenders</h2>
+                    </div>
+                    <p>Offering your guests an exclusive beverage is vital at any event. Let our high energy bartenders customize some flavorful liquid refreshments to create a buzz-worthy experience!</p>
+                    <div class="uk-text-center mt-5">
+                        <a href="" class="uk-btn uk-primary ">Read More</a>
+                    </div>
+                </div>
+
+            </div>
+            <div>
+                <div class="media">
+                    <img src="assets/img/banner.jpg" alt="">
+                </div>
+            </div>
+            <div class=" uk-services-content">
+                <div class="uk-padding uk-flex uk-flex-middle uk-flex-column uk-text-center">
+                    <div class="uk-title ">
+                        <h4 class="uk-margin-remove">Services</h4>
+                    </div>
+                    <div class="uk-section-title uk-text-center">
+                        <h2>Just Bartenders</h2>
+                    </div>
+                    <p>Offering your guests an exclusive beverage is vital at any event. Let our high energy bartenders customize some flavorful liquid refreshments to create a buzz-worthy experience!</p>
+                    <div class="uk-text-center mt-5">
+                        <a href="" class="uk-btn uk-primary ">Read More</a>
+                    </div>
+                </div>
+
+            </div>
+            <div>
+                <div class="media">
+                    <img src="assets/img/banner.jpg" alt="">
+                </div>
+            </div>
+            <div class=" uk-services-content">
+                <div class="uk-padding uk-flex uk-flex-middle uk-flex-column uk-text-center">
+                    <div class="uk-title ">
+                        <h4 class="uk-margin-remove">Services</h4>
+                    </div>
+                    <div class="uk-section-title uk-text-center">
+                        <h2>Just Bartenders</h2>
+                    </div>
+                    <p>Offering your guests an exclusive beverage is vital at any event. Let our high energy bartenders customize some flavorful liquid refreshments to create a buzz-worthy experience!</p>
+                    <div class="uk-text-center mt-5">
+                        <a href="" class="uk-btn uk-primary ">Read More</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+<?php include('assets/include/footer.php') ?>
